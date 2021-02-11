810d620a4f0133fb5b129dc5c0f00f91b664b1fa
Parada <paradafaii28@gmail.com>
2021-02-11 13:56:30 -0700

    study declare varible with 3 different ways

---
+++ assignments.js
@@ -0,0 +1,19 @@
+let country = 'Thailand';
+let continent = 'Chonburi';
+let population = '60,000,000'
+
+console.log(country)
+console.log(continent)
+console.log(population)
+console.log(typeof country)
+
+
+let age =30;
+age = 30;
+
+var job = 'coder';
+job = 'teacher';
+
+// not good idea
+lastName = 'Richardson';
+console.log(lastName);
