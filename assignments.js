7ef35c3f76d8fe37d87b48acc71c4cd6973284d5
Parada <paradafaii28@gmail.com>
2021-02-11 14:30:01 -0700

    learn javascript operator

--- assignments.js
+++ assignments.js
@@ -15,5 +15,21 @@ var job = 'coder';
 job = 'teacher';
 
 // not good idea
-lastName = 'Richardson';
-console.log(lastName);
+// lastName = 'Richardson';
+// console.log(lastName);
+
+
+const now = 2037;
+const ageAlice = now - 1991;
+const ageSara = now - 2018;
+console.log(ageAlice, ageSara);
+
+console.log(ageAlice * 2, ageAlice / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Alice';
+const lastName = 'Richardson';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+console.log(x)
