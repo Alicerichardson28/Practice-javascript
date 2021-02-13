e0c4345be11823602dc9330a3816db15acf59326
Parada <paradafaii28@gmail.com>
2021-02-13 16:17:42 -0700

    learn if-else statment

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ if(js === 'amazing') alert('Javascript is Fun');
 
 console.log(40+8+23-10);
 
-let age =30;
-age = 30;
+// let age =30;
+// age = 30;
 
-var job = 'coder';
-job = 'teacher';
+// var job = 'coder';
+// job = 'teacher';
 
 // not good idea
 // lastName = 'Richardson';
@@ -37,19 +37,29 @@ console.log(firstName + ' ' + lastName);
 // console.log(x);
 
 //Comparison operators
-console.log(ageAlice > ageSara); // comparison operators is the boolean // >, <, >=,<=
-console.log(ageSara >= 18);
-// we will store the results in variables
+// console.log(ageAlice > ageSara); // comparison operators is the boolean // >, <, >=,<=
+// console.log(ageSara >= 18);
+// // we will store the results in variables
 
-const isFullAge = ageSara >= 18;
+// const isFullAge = ageSara >= 18;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
 
 
-let x,y;
-x = y = 25 - 10 - 5 // left to right x = y = 10, = right to left x = 10, y = 10
-console.log(x, y)
+// let x,y;
+// x = y = 25 - 10 - 5 // left to right x = y = 10, = right to left x = 10, y = 10
+// console.log(x, y)
 
-const averageAge = (ageAlice + ageSara) / 2;
-console.log(ageAlice, ageSara, averageAge);
+// const averageAge = (ageAlice + ageSara) / 2;
+// console.log(ageAlice, ageSara, averageAge);
 
+
+const age = 15;
+
+// condition will be a boolean
+if(age >= 18) {
+    console.log('Sara can start apply driving license 🚗 ');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sara is too yound. Wait another ${yearsLeft} years.`)
+}
