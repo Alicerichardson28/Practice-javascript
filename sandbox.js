b693bbcad4ab9e4a429c84e36f7217bc33963f3f
Parada <paradafaii28@gmail.com>
2021-03-25 11:17:22 -0600

    learn if else statement compair operator

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,14 @@
 const favorite = Number(prompt("What is your favorite number?"));
 console.log(favorite)
 console.log(typeof favorite)
+
+if (favorite === 18) {
+    console.log('Cool! 18 is an amazing number!')
+} else if (favorite === 7) {
+    console.log('7 is also a cool number')
+} else {
+    console.log('Number is not 18 or 7')
+}
+
+
+if (favorite !== 18) console.log('Why not 18');
