0a2aacff111f813af6fef566696ebcbd17ef2fc4
Parada <paradafaii28@gmail.com>
2021-03-25 13:00:15 -0600

    code challenge section 2 part #3

--- assignments.js
+++ assignments.js
@@ -1,40 +1,100 @@
-let country = 'Thailand';
-let continent = 'Chonburi';
-let population = '60,000,000'
+// let country = 'Thailand';
+// let continent = 'Chonburi';
+// let population = '60,000,000'
 
-console.log(country)
-console.log(continent)
-console.log(population)
-console.log(typeof country)
+// console.log(country)
+// console.log(continent)
+// console.log(population)
+// console.log(typeof country)
 
 
-//  code challenge 
+// //  code challenge 
 
-// const marksMass = 78;
-// const marksHeight = 1.69;
+// // const marksMass = 78;
+// // const marksHeight = 1.69;
 
-// const johnMass = 92;
-// const johnHeight = 1.95;
+// // const johnMass = 92;
+// // const johnHeight = 1.95;
 
-const marksMass = 95;
-const marksHeight = 1.88;
+// const marksMass = 95;
+// const marksHeight = 1.88;
 
-const johnMass = 85;
-const johnHeight = 1.76;
+// const johnMass = 85;
+// const johnHeight = 1.76;
 
-// Calculate both their BMIS
-const bmiMarksResult = marksMass / marksHeight ** 2;
-const bmiJohnResult = johnMass / (johnHeight * johnHeight);
+// // Calculate both their BMIS
+// const bmiMarksResult = marksMass / marksHeight ** 2;
+// const bmiJohnResult = johnMass / (johnHeight * johnHeight);
 
-// create boolean variable
-const markHigherBMI = (bmiMarksResult > bmiJohnResult);
+// // create boolean variable
+// const markHigherBMI = (bmiMarksResult > bmiJohnResult);
 
-console.log(bmiMarksResult, bmiJohnResult);
-console.log(markHigherBMI);
+// console.log(bmiMarksResult, bmiJohnResult);
+// console.log(markHigherBMI);
 
-// second coding challenge
-if(bmiMarksResult > bmiJohnResult){
-    console.log(`Mark's BMI (${bmiMarksResult}) is higher than John's!`)
+// // second coding challenge
+// if(bmiMarksResult > bmiJohnResult){
+//     console.log(`Mark's BMI (${bmiMarksResult}) is higher than John's!`)
+// } else {
+//     console.log(`John's BMI (${bmiMarksResult}) is higher than Mark's!`)
+// }
+
+
+// const dolphinsScore = (96+ 108 + 89) / 3
+// const koalasScore = (88+ 91 + 110) / 3
+
+// if(dolphinsScore > koalasScore) {
+//     console.log('Dolphins win the trophy');
+// } else if ( koalasScore > dolphinsScore){
+//     console.log('Koalas win the trophy');
+// } else if (dolphinsScore === koalasScore){
+//     console.log('Dolphins and Koalas both win the trophy')
+// }
+
+// const dolphinsScore = (97+ 112 + 81) / 3
+// const koalasScore = (109+ 95 + 86) / 3
+
+
+// console.log(dolphinsScore)
+// console.log(koalasScore)
+
+// if(dolphinsScore > koalasScore && dolphinsScore >= 100) {
+//     console.log('Dolphins win the trophy');
+// } else if ( koalasScore > dolphinsScore && koalasScore >= 100){
+//     console.log('Koalas win the trophy');
+// } else if (dolphinsScore === koalasScore && dolphinsScore >= 100 && koalasScore >=100){
+//     console.log('Dolphins and Koalas both win the trophy')
+// } else {
+//     console.log('No one wins the trophy')
+// }
+
+const dolphinsScore = (97+ 112 + 80) / 3
+const koalasScore = (109+ 95 + 50) / 3
+
+
+console.log(dolphinsScore)
+console.log(koalasScore)
+
+if(dolphinsScore > koalasScore && dolphinsScore >= 100) {
+    console.log('Dolphins win the trophy');
+} else if ( koalasScore > dolphinsScore && koalasScore >= 100){
+    console.log('Koalas win the trophy');
+} else if (dolphinsScore === koalasScore && dolphinsScore >= 100 && koalasScore >=100){
+    console.log('Dolphins and Koalas both win the trophy')
 } else {
-    console.log(`John's BMI (${bmiMarksResult}) is higher than Mark's!`)
+    console.log('No one wins the trophy')
 }
+
+// const dolphinsScore = (97+ 112 + 101) / 3
+// const koalasScore = (109+ 95 + 106) / 3
+
+// console.log(dolphinsScore)
+// console.log(koalasScore)
+
+// if(dolphinsScore > koalasScore) {
+//     console.log('Dolphins win the trophy');
+// } else if ( koalasScore > dolphinsScore){
+//     console.log('Koalas win the trophy');
+// } else if (dolphinsScore === koalasScore){
+//     console.log('Dolphins and Koalas both win the trophy')
+// }
--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,39 @@
-const favorite = Number(prompt("What is your favorite number?"));
-console.log(favorite)
-console.log(typeof favorite)
+// const favorite = Number(prompt("What is your favorite number?"));
+// console.log(favorite)
+// console.log(typeof favorite)
 
-if (favorite === 18) {
-    console.log('Cool! 18 is an amazing number!')
-} else if (favorite === 7) {
-    console.log('7 is also a cool number')
-} else {
-    console.log('Number is not 18 or 7')
+// if (favorite === 18) {
+//     console.log('Cool! 18 is an amazing number!')
+// } else if (favorite === 7) {
+//     console.log('7 is also a cool number')
+// } else {
+//     console.log('Number is not 18 or 7')
+// }
+
+
+// if (favorite !== 18) console.log('Why not 18');
+
+
+const hasDriversLicense = true; //A
+const hasGoodVision = true;//B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if(shouldDrive) {
+//     console.log('Sarah is able to drive.');
+// } else {
+//     console.log('someone else should drive...');
+// }
+
+const isTired = false; //C
+console.log(hasDriversLicense && hasGoodVision && !isTired);
+
+if(hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive.');
+} else  {
+    console.log('someone else should drive...');
 }
-
-
-if (favorite !== 18) console.log('Why not 18');
